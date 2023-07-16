215a3eb1cf6dbb310ad7904fbb4b403a93803ada
jagadeeshftw <jagadeesh26062002@gmail.com>
2023-07-16 21:52:49 +0530

    first component

--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,4 +1,15 @@
-const ExpenseItems=()=>{
-    return(<h2>Expense Item!</h2>);
+import "./ExpenseItems.css";
+
+function ExpenseItems() {
+  return (
+    <div className="expense-item">
+      <div>March 28th 2021</div>
+      <div className="expense-item__description">
+        <h2>Car Insurance</h2>
+        <div className="expense-item__price">$294.67</div>
+      </div>
+    </div>
+  );
 }
+
 export default ExpenseItems;
